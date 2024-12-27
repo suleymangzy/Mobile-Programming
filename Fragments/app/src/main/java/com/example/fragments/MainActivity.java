6f2eed9f58379e35ecad36fdb8f3c00dbfe59a61
suleymangzy <sguzey.mail@gmail.com>
2024-12-27 16:24:24 +0300

    Arrangement...

--- Fragments/app/src/main/java/com/example/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.java
@@ -18,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        b = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+        
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
